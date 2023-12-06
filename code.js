aad2cb6a40d09248382f3ff396a47bd6144a26df
boboestes14 <143558849+boboestes14@users.noreply.github.com>
2023-12-06 03:08:57 +0000

    commit

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
 function augmentingPath(graph, start, end) {
-    return [];
+    graph[start].v = "v"
+    const ke = Object.keys(graph[start])
+    if(start == end){
+        return [end];
+    }
+    for(n = 0; n < ke.length-1; n++){
+        if(graph[ke[n]]['v'] != "v"){
+            a = [start].concat(augmentingPath(graph, ke[n], end))
+            if(a[a.length-1] != -1){
+                return a}
+            a.pop()
+        }
+    }
+    n--
+    n--
+    return [-1];
 }
